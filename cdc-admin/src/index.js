8e259159331ad143b6e9b1d02cf4220bbdfddbbd
Bruno Anhaia <hiroki.zx@gmail.com>
2019-01-13 12:40:45 -0200

    6-6

--- cdc-admin/src/index.js
+++ cdc-admin/src/index.js
@@ -3,8 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {Router,Route,browserHistory, IndexRoute} from 'react-router';
+import AutorBox from './modules/Autor';
+import Home from './modules/Home';
+ 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    (<Router history={browserHistory}>
+        <Route path="/" component={App}>
+            <IndexRoute component={Home}/>
+            <Route path="/autor" component={AutorBox}/>
+            <Route path="/livro"/>
+        </Route>
+
+      </Router>)
+    , document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
